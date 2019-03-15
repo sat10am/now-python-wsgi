b1ff601e13bc46f033f99a6451540e797d360d18
James Hale <jameshale@pm.me>
2019-03-15 10:16:16 -0400

    Removing unused all_casings from handler

--- now_python_wsgi/handler.py
+++ now_python_wsgi/handler.py
@@ -44,25 +44,6 @@ TEXT_MIME_TYPES = [
 ]
 
 
-def all_casings(input_string):
-    """
-    Permute all casings of a given string.
-    A pretty algoritm, via @Amber
-    http://stackoverflow.com/questions/6792803
-    """
-    if not input_string:
-        yield ""
-    else:
-        first = input_string[:1]
-        if first.lower() == first.upper():
-            for sub_casing in all_casings(input_string[1:]):
-                yield first + sub_casing
-        else:
-            for sub_casing in all_casings(input_string[1:]):
-                yield first.lower() + sub_casing
-                yield first.upper() + sub_casing
-
-
 def handler(app, lambda_event, context):
 
     event = json.loads(lambda_event['body'])
