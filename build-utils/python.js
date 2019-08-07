d3a63fbc2c8e7ffbf01448be54d3471731e08fac
James Hale <jameshale@pm.me>
2019-08-07 17:04:38 -0600

    Fix IUS repo install

--- build-utils/python.js
+++ build-utils/python.js
@@ -1,40 +1,12 @@
 const { sync: commandExists } = require('command-exists');
-const execa = require('execa');
 
 const log = require('./log');
 
 
 const runtimeBinaryMap = {
   'python2.7': 'python27',
-  'python3.6': 'python36',
+  'python3.6': 'python3.6',
 };
-
-const runtimePrebuiltMap = {
-  'python3.6': {
-    repo: 'https://centos6.iuscommunity.org/ius-release.rpm',
-    binaryName: 'python3.6',
-    packageName: 'python36u',
-  },
-};
-
-
-async function downloadAndInstallPython(runtime) {
-  log.subheading('Installing python from package');
-
-  if (!(runtime in runtimePrebuiltMap)) {
-    throw new Error(`Installing python is not supported for ${runtime}`);
-  }
-
-  const { binaryName, packageName, repo } = runtimePrebuiltMap[runtime];
-
-  const repoInstall = await execa('yum', ['install', '-y', repo]);
-  log.info(repoInstall.stdout);
-
-  const packInstall = await execa('yum', ['install', '-y', packageName]);
-  log.info(packInstall.stdout);
-
-  return binaryName;
-}
 
 
 async function findPythonBinary(runtime) {
@@ -48,8 +20,7 @@ async function findPythonBinary(runtime) {
     return binaryName;
   }
 
-  // Attempt to build from source if the binary is not available
-  return downloadAndInstallPython(runtime);
+  throw new Error(`Unable to find binary ${binaryName} for runtime ${runtime}`);
 }
 
 
