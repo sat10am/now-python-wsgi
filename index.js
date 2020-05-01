f9e59ad546ae4015226fe3a27c8ec5fe9453b5e2
sat10am-dev <64578264+sat10am-dev@users.noreply.github.com>
2020-05-01 16:18:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,6 @@ exports.config = {
 
 exports.build = async ({ files, entrypoint, config }) => {
   log.title('Starting build');
-  const systemReleaseContents = await readFile(
-    path.join('/etc', 'system-release'),
-    'utf8',
-  );
-  log.info(`Build AMI version: ${systemReleaseContents.trim()}`);
 
   const runtime = config.runtime || 'python3.6';
   python.validateRuntime(runtime);
